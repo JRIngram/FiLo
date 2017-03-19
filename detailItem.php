1cd76ee1cca60ab4f7ede265672ccf29203e8ac8
Jringram <jr-ingram@hotmail.co.uk>
2017-03-19 15:18:49 +0000

    Added details for specific item types

--- detailItem.php
+++ detailItem.php
@@ -25,6 +25,44 @@
         <p><b>Found Date: </b> <?php echo $item["found_date"]; ?></p>
         <p><b>Found Place: </b> <?php echo $item["found_place"]; ?></p>
         <p><b>Main Colour: </b> <?php echo $item["colour"]; ?></p>
+        <?php
+          #Creates specific questions.
+          if($item["category"] == "pet"){
+            #Add Pet questions
+            $petDetails = $db->query('SELECT * FROM pet WHERE item_id = ' . $item["item_id"]);
+            $pet = $petDetails->fetch();
+        ?>
+        <p><b>Animal: </b> <?php echo $pet["animal"]?></p>
+        <p><b>Pet Name: </b> <?php echo $pet["pet_name"] ?></p>
+        <p><b>Breed: </b> <?php echo $pet["breed"] ?></p>
+        <p><b>Collar Colour: </b> <?php echo $pet["collar_colour"]?></p>
+
+        <?php
+          }
+          elseif($item["category"] == "electronic"){
+            #Add electronic questions
+            $electronicDetails = $db->query('SELECT * FROM electronic WHERE item_id = ' . $item["item_id"]);
+            $electronic = $electronicDetails->fetch();
+        ?>
+        <p><b>Electronic Type: </b> <?php echo $electronic["electronicType"] ?></p>
+        <p><b>Brand: </b> <?php echo $electronic["brand"] ?></p>
+        <p><b>Model: </b> <?php echo $electronic["model"] ?></p>
+
+        <?php
+          }
+          elseif($item["category"] == "jewellery"){
+            #Add jewellery questions
+            $jewelleryDetails = $db->query('SELECT * FROM jewellery WHERE item_id = ' . $item["item_id"]);
+            $jewellery = $jewelleryDetails->fetch();
+        ?>
+        <p><b>Metal: </b><?php echo $jewellery["metal"] ?></p>
+        <p><b>Jewellery Type: </b><?php echo $jewellery["jewellery_type"] ?></p>
+        <?php
+          }
+          else{
+            echo "<p><b>ERROR</b>Invalid Item Type</p>";
+          }
+        ?>
         <p><b>Description: <br/></b><p> <?php echo $item["description"] ?></p>
       </div>
       <div class="col-md-4">
